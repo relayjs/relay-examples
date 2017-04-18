7f4c7ad357de494be5c42e19598b966075adac32
Jan Kassens <jan@kassens.net>
2017-04-18 10:48:47 -0700

    Use schema.graphql instead of schema.json

--- todo-modern/scripts/updateSchema.js
+++ todo-modern/scripts/updateSchema.js
@@ -14,27 +14,10 @@
 import fs from 'fs';
 import path from 'path';
 import { schema } from '../data/schema';
-import { graphql } from 'graphql';
-import { introspectionQuery, printSchema } from 'graphql/utilities';
+import { printSchema } from 'graphql';
 
-// Save JSON of full schema introspection for Babel Relay Plugin to use
-(async () => {
-  const result = await (graphql(schema, introspectionQuery));
-  if (result.errors) {
-    console.error(
-      'ERROR introspecting schema: ',
-      JSON.stringify(result.errors, null, 2)
-    );
-  } else {
-    fs.writeFileSync(
-      path.join(__dirname, '../data/schema.json'),
-      JSON.stringify(result, null, 2)
-    );
-  }
-})();
+const schemaPath = path.resolve(__dirname, '../data/schema.graphql');
 
-// Save user readable type system shorthand of schema
-fs.writeFileSync(
-  path.join(__dirname, '../data/schema.graphql'),
-  printSchema(schema)
-);
+fs.writeFileSync(schemaPath, printSchema(schema));
+
+console.log('Wrote ' + schemaPath);
