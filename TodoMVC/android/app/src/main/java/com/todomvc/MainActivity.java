aeffe0848ef5286737b3b021eb1ae6bffa79ef14
Jan Kassens <jan@kassens.net>
2017-07-12 21:22:06 -0700

    [TodoMVC] update react-native

--- TodoMVC/android/app/src/main/java/com/todomvc/MainActivity.java
+++ TodoMVC/android/app/src/main/java/com/todomvc/MainActivity.java
@@ -18,23 +18,4 @@ public class MainActivity extends ReactActivity {
         return "TodoMVC";
     }
 
-    /**
-     * Returns whether dev mode should be enabled.
-     * This enables e.g. the dev menu.
-     */
-    @Override
-    protected boolean getUseDeveloperSupport() {
-        return BuildConfig.DEBUG;
-    }
-
-    /**
-     * A list of packages used by the app. If the app uses additional views
-     * or modules besides the default ones, add more packages here.
-     */
-    @Override
-    protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-            new MainReactPackage()
-        );
-    }
 }
