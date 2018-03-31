b8343b60c7646a860f38b6304eddf9d426e5eadb
Jan Kassens <jan@kassens.net>
2018-03-30 23:17:27 -0700

    [todo] serve GraphQL and js from same port
    This avoids the extra proxying and seems easier to follow.

--- todo/server.js
+++ todo/server.js
@@ -18,14 +18,6 @@ import WebpackDevServer from 'webpack-dev-server';
 import {schema} from './data/schema';
 
 const APP_PORT = 3000;
-const GRAPHQL_PORT = 8080;
-
-// Expose a GraphQL endpoint
-const graphQLServer = express();
-graphQLServer.use('/', graphQLHTTP({schema, pretty: true}));
-graphQLServer.listen(GRAPHQL_PORT, () => console.log(
-  `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}`
-));
 
 // Serve the Relay app
 const compiler = webpack({
@@ -45,16 +37,26 @@ const compiler = webpack({
       }
     ]
   },
-  output: {filename: 'app.js', path: '/'},
+  output: {
+    filename: 'app.js',
+    path: '/',
+  },
 });
 const app = new WebpackDevServer(compiler, {
   contentBase: '/public/',
-  proxy: {'/graphql': `http://localhost:${GRAPHQL_PORT}`},
   publicPath: '/js/',
   stats: {colors: true},
 });
+
 // Serve static resources
 app.use('/', express.static(path.resolve(__dirname, 'public')));
+
+// Setup graphql endpoint
+app.use('/graphql', graphQLHTTP({
+  schema: schema,
+  pretty: true,
+}));
+
 app.listen(APP_PORT, () => {
   console.log(`App is now running on http://localhost:${APP_PORT}`);
 });
