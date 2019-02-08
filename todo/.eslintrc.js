6cad12648c64b0633b402176c7c5b199c562da22
Stephen Pittman <stephen@sig.ma>
2019-02-07 20:57:52 -0800

    Add eslint and fail CI on lint error
    Removes prettier as an npm script, due to integration into eslint
    Running 'yarn run lint --fix' will also fix prettier issues

---
+++ todo/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: [
+    'fbjs/strict',
+    'plugin:flowtype/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:relay/recommended',
+    'prettier/flowtype',
+    'prettier/react',
+  ],
+  parser: 'babel-eslint',
+  plugins: ['babel', 'flowtype', 'prettier', 'react', 'relay'],
+  rules: {
+    'no-console': 'off',
+    'one-var': 'off',
+    'react/prop-types': 'off', // Replaced by flow types
+    'flowtype/require-variable-type': 'off', // Temporarily
+  },
+  settings: {
+    react: {
+      version: '16.8.1',
+      flowVersion: '0.92.1',
+    },
+  },
+};
