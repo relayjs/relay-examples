a0d0323bf1e7844d317c45ffce4515661b03cff9
Stephen Pittman <stephen@sig.ma>
2019-03-12 16:47:25 -0700

    Add strict flowtype lint rules

--- todo/.eslintrc.js
+++ todo/.eslintrc.js
@@ -14,15 +14,34 @@ module.exports = {
     'no-console': 'off',
     'one-var': 'off',
     'react/prop-types': 'off', // Replaced by flow types
-    'flowtype/require-variable-type': 'off', // Temporarily
 
     // Mutations aren't located in the same file as Components
     'relay/unused-fields': 'off',
+    
+    // Strict Flow linting
+    'flowtype/array-style-complex-type': 'error',
+    'flowtype/array-style-simple-type': 'error',
+    'flowtype/newline-after-flow-annotation': ['error', 'never'],
+    'flowtype/no-dupe-keys': 'error',
+    'flowtype/no-existential-type': 'error',
+    'flowtype/no-mutable-array': 'error',
+    'flowtype/no-primitive-constructor-types': 'error',
+    'flowtype/no-unused-expressions': 'error',
+    'flowtype/no-weak-types': 'error',
+    'flowtype/require-parameter-type': 'error',
+    'flowtype/require-return-type': 'off', // Too strict
+    'flowtype/require-types-at-top': 'error',
+    'flowtype/require-valid-file-annotation': 'error',
+    'flowtype/require-variable-type': 'off', // Too strict
+    'flowtype/sort-keys': 'error',
   },
   settings: {
     react: {
       version: '16.8.1',
       flowVersion: '0.94.0',
     },
+    flowtype: {
+      onlyFilesWithFlowAnnotation: true, // TODO: turn this setting off once @flow is everywhere
+    }
   },
 };
