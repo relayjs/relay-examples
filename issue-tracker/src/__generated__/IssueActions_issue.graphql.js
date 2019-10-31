09866d036ca94335938e8b5bed355a87814533ef
Joseph Savona <josephsavona@users.noreply.github.com>
2019-10-31 14:34:17 -0700

    Support opening/closing issues (#110)

--- issue-tracker/src/__generated__/IssueActions_issue.graphql.js
+++ issue-tracker/src/__generated__/IssueActions_issue.graphql.js
@@ -13,6 +13,7 @@ declare export opaque type IssueActions_issue$ref: FragmentReference;
 declare export opaque type IssueActions_issue$fragmentType: IssueActions_issue$ref;
 export type IssueActions_issue = {|
   +id: string,
+  +closed: boolean,
   +$refType: IssueActions_issue$ref,
 |};
 export type IssueActions_issue$data = IssueActions_issue;
@@ -36,8 +37,15 @@ const node /*: ReaderFragment*/ = {
       args: null,
       storageKey: null,
     },
+    {
+      kind: 'ScalarField',
+      alias: null,
+      name: 'closed',
+      args: null,
+      storageKey: null,
+    },
   ],
 };
 // prettier-ignore
-(node/*: any*/).hash = 'd5685c9da1b2a8edf75a49d1b58a22ab';
+(node/*: any*/).hash = '12c79d27df99eb9656621cead33c9d08';
 module.exports = node;
