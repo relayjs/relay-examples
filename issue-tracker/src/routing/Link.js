e085ba03df976a7bd23540dffde0fa94a1085a06
Joe Savona <joesavona@fb.com>
2019-11-01 11:57:18 -0700

    Preload routes in <Link> on hover/mousedown

--- issue-tracker/src/routing/Link.js
+++ issue-tracker/src/routing/Link.js
@@ -9,16 +9,26 @@ const { useCallback, useContext } = React;
  */
 export default function Link(props) {
   const router = useContext(RoutingContext);
-  const { history } = router;
-  const onClick = useCallback(
+  const changeRoute = useCallback(
     event => {
       event.preventDefault();
-      history.push(props.to);
+      router.history.push(props.to);
     },
-    [props.to, history],
+    [props.to, router],
   );
+  const preloadRouteCode = useCallback(() => {
+    router.preloadCode(props.to);
+  }, [props.to, router]);
+  const preloadRoute = useCallback(() => {
+    router.preload(props.to);
+  }, [props.to, router]);
   return (
-    <a href={props.to} onClick={onClick}>
+    <a
+      href={props.to}
+      onClick={changeRoute}
+      onMouseEnter={preloadRouteCode}
+      onMouseDown={preloadRoute}
+    >
       {props.children}
     </a>
   );
