85164d219b9fa65a1de4d91acedf038b88d3ddf1
Joe Savona <joesavona@fb.com>
2019-11-06 14:05:14 -0800

    useMutation helper to simplify IssueActions

--- issue-tracker/src/IssueActions.js
+++ issue-tracker/src/IssueActions.js
@@ -1,9 +1,10 @@
 import graphql from 'babel-plugin-relay/macro';
 import React from 'react';
-import { useFragment, useRelayEnvironment } from 'react-relay/hooks';
-import { commitMutation, ConnectionHandler } from 'relay-runtime';
+import { useFragment } from 'react-relay/hooks';
+import { ConnectionHandler } from 'relay-runtime';
+import useMutation from './useMutation';
 
-const { useCallback, useState, useEffect, useRef } = React;
+const { useCallback, useState } = React;
 
 const AddCommentMutation = graphql`
   mutation IssueActionsAddCommentMutation($input: AddCommentInput!) {
@@ -47,25 +48,13 @@ const ReopenIssueMutation = graphql`
 `;
 
 export default function IssueActions(props) {
-  // Get the active Relay environment; developers should always prefer to use the environment
-  // from context rather than a singleton, as this keeps the app flexible. For example, this
-  // allows you to render part of a UI in a different environment.
-  const environment = useRelayEnvironment();
-
   // Track the current comment text - this is used as the value of the comment textarea
   const [commentText, setCommentText] = useState('');
 
-  // We want to avoid sending multiple requests in parallel. Using state to guard against this
-  // isn't reliable in concurrent mode, as state updates are batched. We therefore keep call
-  // both a ref (pendingMutationRef) and state (isPending). The ref ensures we know for sure
-  // if a mutation is pending, while the state is a (slightly) delayed representation that is
-  // displayed to the user.
-  //
-  // NOTE: this is a pattern that you'd typically push to a shared infrastructure. Relay Hooks
-  // doesn't currently have a `useMutation()` hook - yet! - but when it does, it would implement
-  // a pattern like the one here, so product developers don't have to think about it.
-  const [isPending, setPending] = useState(false);
-  const pendingMutationRef = useRef(null);
+  const [isCommentPending, addComment] = useMutation(AddCommentMutation);
+  const [isClosePending, closeIssue] = useMutation(CloseIssueMutation);
+  const [isReopenPending, reopenIssue] = useMutation(ReopenIssueMutation);
+  const isPending = isCommentPending || isClosePending || isReopenPending;
 
   // Get the data we need about the issue in order to execute the mutation. Right now that's just
   // the id, but in the future this component might neeed more information.
@@ -92,14 +81,7 @@ export default function IssueActions(props) {
   const onSubmit = useCallback(
     event => {
       event.preventDefault();
-
-      // If there's already a mutation pending bail out
-      if (pendingMutationRef.current != null) {
-        return;
-      }
-      // Run the mutation
-      const request = commitMutation(environment, {
-        mutation: AddCommentMutation,
+      addComment({
         variables: {
           input: {
             body: commentText,
@@ -134,29 +116,11 @@ export default function IssueActions(props) {
             null, // we can specify a cursor value here to insert the new edge after that  cursor
           );
         },
-        onCompleted: () => {
-          // When the mutation completes immediately clear the ref
-          pendingMutationRef.current = null;
-          // And also update state - note that this will be slightly delayed
-          setPending(false);
-        },
-        onError: errors => {
-          // In a real app we'd show the errors to the user in the UI, for simplicity
-          // we just log to the console.
-          console.error(errors);
-          // When the mutation completes immediately clear the ref
-          pendingMutationRef.current = null;
-          // And also update state - note that this will be slightly delayed
-          setPending(false);
-        },
       });
-      // Immediately update the ref with the current request
-      pendingMutationRef.current = request;
-      // Reset the comment text and mark the mutation as pending
+      // Reset the comment text
       setCommentText('');
-      setPending(true);
     },
-    [environment, commentText, setCommentText, issueId, pendingMutationRef],
+    [commentText, setCommentText, issueId, addComment],
   );
 
   // Reopen/Close the issue
@@ -164,50 +128,22 @@ export default function IssueActions(props) {
     event => {
       event.preventDefault();
 
-      // If there's already a mutation pending bail out
-      if (pendingMutationRef.current != null) {
-        return;
-      }
-
       // Switch mutation based on the current open/close status
-      const mutation = data.closed ? ReopenIssueMutation : CloseIssueMutation;
-
-      const request = commitMutation(environment, {
-        mutation,
+      const config = {
         variables: {
           input: {
             issueId: data.id,
           },
         },
-        onCompleted: () => {
-          pendingMutationRef.current = null;
-          setPending(false);
-        },
-        onError: errors => {
-          console.error(errors);
-          pendingMutationRef.current = null;
-          setPending(false);
-        },
-        // note: there's no need for an updater() function here, Relay merges fields
-        // for objects that have an `id`
-      });
-      // Immediately update the ref with the current request
-      pendingMutationRef.current = request;
-      // Mark the mutation as pending
-      setPending(true);
-    },
-    [environment, data, pendingMutationRef],
-  );
-
-  useEffect(() => {
-    // Cancel the mutation when the component unmounts; this is necessary to
-    // avoid attempting to modify the unmountd component (e.g. w setPending)
-    return () => {
-      if (pendingMutationRef.current != null) {
-        pendingMutationRef.current.dispose();
+      };
+      if (data.closed) {
+        reopenIssue(config);
+      } else {
+        closeIssue(config);
       }
-    };
-  }, [pendingMutationRef]);
+    },
+    [data, reopenIssue, closeIssue],
+  );
 
   return (
     <form onSubmit={onSubmit} className="issue-actions">
