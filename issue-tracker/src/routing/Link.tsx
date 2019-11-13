a8424c6228b2168c0c211d63485fc6b2457ba9c4
Renan Machado <renan.mav@hotmail.com>
2019-11-13 13:21:25 -0300

    throw if router doesnt exists on Link and remove unnecessary chainings & returns

--- issue-tracker/src/routing/Link.tsx
+++ issue-tracker/src/routing/Link.tsx
@@ -14,11 +14,14 @@ interface Props {
 export default function Link(props: Props) {
   const router = useContext(RoutingContext);
 
+  if (router == null)
+    throw new Error('<Link> requires a routing context to be set.');
+
   // When the user clicks, change route
   const changeRoute = useCallback(
     (event: React.MouseEvent) => {
       event.preventDefault();
-      return router?.history.push(props.to);
+      router.history.push(props.to);
     },
     [props.to, router],
   );
@@ -27,14 +30,14 @@ export default function Link(props: Props) {
   // we pass this to onMouseEnter, which is a weaker signal
   // that the user *may* navigate to the route.
   const preloadRouteCode = useCallback(() => {
-    return router?.preloadCode(props.to);
+    router.preloadCode(props.to);
   }, [props.to, router]);
 
   // Callback to preload the code and data for the route:
   // we pass this to onMouseDown, since this is a stronger
   // signal that the user will likely complete the navigation
   const preloadRoute = useCallback(() => {
-    return router?.preload(props.to);
+    router.preload(props.to);
   }, [props.to, router]);
 
   return (
