2d8331dbcb74e7b98b962c6139b5b2e6d5cdc6aa
Jan Kassens <jan@kassens.net>
2020-02-07 16:06:40 -0500

    Update flow-typed defs
    By running `yarn run flow-typed`.

--- todo/flow-typed/npm/classnames_v2.x.x.js
+++ todo/flow-typed/npm/classnames_v2.x.x.js
@@ -1,9 +1,9 @@
-// flow-typed signature: cf86673cc32d185bdab1d2ea90578d37
-// flow-typed version: 614bf49aa8/classnames_v2.x.x/flow_>=v0.25.x
+// flow-typed signature: a00cf41b09af4862583460529d5cfcb9
+// flow-typed version: c6154227d1/classnames_v2.x.x/flow_>=v0.104.x
 
 type $npm$classnames$Classes =
   | string
-  | { [className: string]: * }
+  | { [className: string]: *, ... }
   | false
   | void
   | null;
