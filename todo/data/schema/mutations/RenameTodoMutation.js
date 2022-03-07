e48ac5fa05ed0417438ef8b413e6cf8600b2ae6f
voideanvalue <voideanvalue@users.noreply.github.com>
2022-03-07 20:28:55 +0000

    remove license from file headers

--- todo/data/schema/mutations/RenameTodoMutation.js
+++ todo/data/schema/mutations/RenameTodoMutation.js
@@ -1,17 +1,6 @@
 // @flow
 /* graphql-relay doesn't export types, and isn't in flow-typed.  This gets too messy */
 /* eslint flowtype/require-return-type: 'off' */
-/**
- * This file provided by Facebook is for non-commercial testing and evaluation
- * purposes only.  Facebook reserves all rights not expressly granted.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 
 import {mutationWithClientMutationId, fromGlobalId} from 'graphql-relay';
 import {
