43ed9c5fea14518a25091b61433d832673e2e88e
Andrey Lunyov <alunyov@meta.com>
2022-12-08 12:07:07 -0500

    process graphql requests on the server

--- newsfeed/server/index.mjs
+++ newsfeed/server/index.mjs
@@ -1,10 +1,31 @@
 import http from 'http';
+import graphql from 'graphql';
+import {schema, rootValue} from './graphql.mjs';
 
 const PORT = 8080;
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   console.log('request received: ' + req.url);
   res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify({data: 'Hello, World!222'}));
+
+  let response = {data: null};
+  if (req.method === 'POST') {
+    const buffers = [];
+    for await (const chunk of req) {
+      buffers.push(chunk);
+    }
+    const requestParams = JSON.parse(Buffer.concat(buffers).toString());
+    response = await graphql({
+      schema,
+      rootValue,
+      source: requestParams.query,
+      variableValues: requestParams.variables,
+    });
+  }
+  if (response?.errors != null) {
+    console.error('GraphQL Server Errors', response.errors);
+  }
+
+  res.end(JSON.stringify(response));
 });
 
 console.log('Starting server...');
