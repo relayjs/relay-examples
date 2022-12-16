5c99d79e4d66f80c2896aafa6f09ea97a496c253
Andrey Lunyov <alunyov@meta.com>
2022-12-16 07:46:23 -0500

    fix typescript error in example

--- newsfeed/src/components/Newsfeed.tsx
+++ newsfeed/src/components/Newsfeed.tsx
@@ -6,11 +6,11 @@ export default function Newsfeed() {
     title: "Why did the chicken cross the road? To get to the other side!",
     summary:
       "But let's be real, we all know the real reason the chicken crossed the road: to take a break from its busy day and enjoy the scenery on the other side. Sorry, other animals, but the chicken's gotta do what the chicken's gotta do..\n\nOk, ok, let's get even more real. The chicken didn't actually cross the road. It was just a clever disguise to avoid being cooked for dinner. Smart chicken, indeed.\n\nBut really, at the end of the day, does it even matter why the chicken crossed the road? All that matters is that it made for a hilarious dad joke that we can all enjoy. So here's to you, chicken. Keep on doing your thing.",
-    poster: {
+    author: {
       name: "Chris P. Bacon",
-      profile_pic: {
+      profilePicture: {
         url: "/assets/chris.p.bacon.png",
-      }
+      },
     },
     thumbnail: {
       url: "/assets/chicken.png",
