ee7347f7a9c94e69679e69ebe6db44d35e792dde
Dave McCabe <davemccabe@fb.com>
2022-12-16 18:58:27 -0800

    More changes for consistency

--- newsfeed/src/components/CategorySelector.tsx
+++ newsfeed/src/components/CategorySelector.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import SmallSpinner from "./SmallSpinner";
 
+// In a real app this would be imported from NewsfeedQuery.graphql; it is only defined
+// separately here to make the step-by-step examples work when partially completed.
 type Category = "ALL" | "COOKING" | "EDUCATION" | "NEWS" | "%future added value";
 
 export default function CategorySelector({
--- newsfeed/src/components/Story.tsx
+++ newsfeed/src/components/Story.tsx
@@ -3,7 +3,6 @@ import Card from "./Card";
 import Heading from "./Heading";
 import PersonalByline, { type Props as PosterProps } from "./PersonalByline";
 import StorySummary from "./StorySummary";
-import Timestamp from "./Timestamp";
 import Image from "./Image";
 
 type Props = {
@@ -22,7 +21,6 @@ export default function Story({ story }: Props): React.ReactElement {
     <Card>
       <PersonalByline person={story.author} />
       <Heading>{story.title}</Heading>
-      <Timestamp time={new Date()} />
       <Image image={story.thumbnail.url} description={story.title} width={400} height={400} />
       <StorySummary summary={story.summary} />
     </Card>
