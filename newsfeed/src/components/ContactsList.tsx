fc991c43548f9638bc8b22be6c33eec5ef02faaa
Dave McCabe <davemccabe@fb.com>
2022-12-20 21:17:31 -0800

    further contact list changes

--- newsfeed/src/components/ContactsList.tsx
+++ newsfeed/src/components/ContactsList.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
-import Image from './Image';
 import {graphql} from 'relay-runtime';
 import {useFragment} from 'react-relay';
 import type {ContactsListFragment$key} from './__generated__/ContactsListFragment.graphql';
-
+import Card from './Card';
+import ContactRow from './ContactRow';
 
 export type Props = {
   viewer: ContactsListFragment$key;
@@ -12,11 +12,7 @@ export type Props = {
 const ContactsListFragment = graphql`
   fragment ContactsListFragment on Viewer {
     contacts {
-      id
-      name
-      profilePicture {
-        ...ImageFragment
-      }
+      ...ContactRowFragment
     }
   }
 `;
@@ -24,11 +20,8 @@ const ContactsListFragment = graphql`
 export default function ContactsList({ viewer }: Props) {
   const data = useFragment(ContactsListFragment, viewer);
   return (
-    data.contacts.map(contact => (
-      <div className="byline" key={contact.id}>
-        <Image image={contact.profilePicture} width={60} height={60} className="byline__image" />
-        <div className="byline__name">{contact.name}</div>
-      </div>
-    ))
+    <Card>
+      {data.contacts.map(contact => <ContactRow contact={contact} />)}
+    </Card>
   );
 }
