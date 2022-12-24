f1e351f59114b13551afad87b26c0d1dab8cb7bb
Dave McCabe <davemccabe@fb.com>
2022-12-23 18:49:30 -0800

    disable comments button

--- newsfeed/src/components/LoadMoreCommentsButton.tsx
+++ newsfeed/src/components/LoadMoreCommentsButton.tsx
@@ -2,14 +2,17 @@ import * as React from "react";
 
 type Props = {
   onClick: () => void;
+  disabled?: boolean;
 };
 
 export default function Comment({
   onClick,
+  disabled,
 }: Props): React.ReactElement {
   return  (
     <button
       onClick={onClick}
+      disabled={disabled}
       className="comments__loadMore">
         Load more comments
     </button>
