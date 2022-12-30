e789d343b711717873de0aae2343eba1ad40f345
Dave McCabe <davemccabe@fb.com>
2022-12-30 14:32:05 -0800

    Render empty story rather than crash when story is null

--- newsfeed/src/components/PosterByline.tsx
+++ newsfeed/src/components/PosterByline.tsx
@@ -11,6 +11,9 @@ export type Props = {
 };
 
 export default function PosterByline({ poster }: Props): React.ReactElement {
+  if (poster == null) {
+    return null;
+  }
   return (
     <div className="byline">
       <Image image={poster.profilePicture} width={60} height={60} className="byline__image" />
