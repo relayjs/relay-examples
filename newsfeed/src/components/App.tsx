7de827acddf5dbb55fc39b36d7f6dad4b673e60e
Dave McCabe <davemccabe@fb.com>
2022-12-29 18:35:38 -0800

    Removed exampleFetch 2

--- newsfeed/src/components/App.tsx
+++ newsfeed/src/components/App.tsx
@@ -1,16 +1,10 @@
 import * as React from "react";
-// import { exampleFetch } from "../relay/example";
 import RelayEnvironment from "../relay/RelayEnvironment";
 import Newsfeed from "./Newsfeed";
 import LoadingSpinner from './LoadingSpinner';
 import Sidebar from "./Sidebar";
 
 export default function App(): React.ReactElement {
-  // Uncomment to test the fetch
-  // React.useEffect(() => {
-  //   exampleFetch().then((data) => console.log("data", data));
-  // }, []);
-
   return (
     <RelayEnvironment>
       <React.Suspense fallback={<LoadingSpinner />}>
