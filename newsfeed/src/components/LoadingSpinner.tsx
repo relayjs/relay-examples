008bfa43dce1ee7dcd7aff7b497886a9cbac3680
Dave McCabe <davemccabe@fb.com>
2023-01-01 20:31:24 -0800

    Improve search experience

--- newsfeed/src/components/LoadingSpinner.tsx
+++ newsfeed/src/components/LoadingSpinner.tsx
@@ -2,6 +2,10 @@ import * as React from "react";
 
 export default function LoadingSpinner({}): React.ReactElement {
   return (
-    <div className="loading-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    <div className="loading-spinner-wrapper">
+      <div className="loading-spinner">
+        <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+      </div>
+    </div>
   );
 }
