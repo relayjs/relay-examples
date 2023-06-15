f9f2eb9b837acc361c35cf1e8f22292a0171ad86
Tom Conroy <tom@conroy.com.au>
2023-06-15 15:28:06 -0400

    Refactor to simplify pattern

--- issue-tracker-next-v13/src/components/MainView.tsx
+++ issue-tracker-next-v13/src/components/MainView.tsx
@@ -1,32 +1,35 @@
-import { Suspense } from "react";
-import { graphql, PreloadedQuery, usePreloadedQuery } from "react-relay";
+"use client";
+
+import { graphql, usePreloadedQuery } from "react-relay";
+import { SerializablePreloadedQuery } from "src/relay/loadSerializableQuery";
+import useSerializablePreloadedQuery from "src/relay/useSerializablePreloadedQuery";
 import { MainViewQuery } from "__generated__/MainViewQuery.graphql";
 import Issues from "./Issues";
 
-export default function MainView(props: {
-  queryRef: PreloadedQuery<MainViewQuery>;
-}) {
-  const data = usePreloadedQuery(
-    graphql`
-      query MainViewQuery($owner: String!, $name: String!) {
-        repository(owner: $owner, name: $name) {
-          owner {
-            login
-          }
-          name
-          ...IssuesFragment
-        }
+export const query = graphql`
+  query MainViewQuery($owner: String!, $name: String!) {
+    repository(owner: $owner, name: $name) {
+      owner {
+        login
       }
-    `,
-    props.queryRef
-  );
+      name
+      ...IssuesFragment
+    }
+  }
+`;
+
+export default function MainView(props: {
+  preloadedQuery: SerializablePreloadedQuery<MainViewQuery>;
+}) {
+  const queryRef = useSerializablePreloadedQuery(props.preloadedQuery);
+  const data = usePreloadedQuery(query, queryRef);
 
   return (
-    <Suspense fallback="Loading (client side)...">
+    <div>
       <h1>
         {data.repository?.owner.login}/{data.repository?.name}
       </h1>
       <Issues repository={data.repository} />
-    </Suspense>
+    </div>
   );
 }
